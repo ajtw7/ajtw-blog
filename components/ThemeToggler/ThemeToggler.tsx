cc5e9453d5692017b71ef1960aab4e71a16e8004
ajtw7 <andrew.worsley91@gmail.com>
2024-06-22 11:50:17 -0400

    Added ThemeToggler w/ index file.

---
+++ components/ThemeToggler/ThemeToggler.tsx
@@ -0,0 +1,11 @@
+// import {React} from 'react';
+
+export default function ThemeToggler() {
+    return (
+        <>
+            <h1>Theme Toggle</h1>
+            <h1>PRAISE JESUS</h1>
+        </>
+    );
+};
+
