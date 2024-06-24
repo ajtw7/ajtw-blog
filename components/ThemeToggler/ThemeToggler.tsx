86d0c76ec66b7b70d700944c3d39ab7926c6b6fe
ajtw7 <andrew.worsley91@gmail.com>
2024-06-24 14:06:01 -0400

    Updating with theme toggler set to preferred themes. Inserted into Intro componenet. Blog name updated in constraints file. Specific styles added to config.

--- components/ThemeToggler/ThemeToggler.tsx
+++ components/ThemeToggler/ThemeToggler.tsx
@@ -1,11 +1,33 @@
-// import {React} from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function ThemeToggler() {
+    const [themeMode, setThemeMode] = useState<string | undefined>(typeof window !== 'undefined' ? localStorage.getItem('themeMode') : undefined);
+    const [theme, setTheme] = useState(themeMode);
+    const [toggle, setToggle] = useState(false);
+
+    const toggleThemeHandler = (e) => {
+        setTheme(toggle === false ? 'luxury' : 'bumblebee');
+        setThemeMode(e.target.value);
+        window.localStorage.setItem('themeMode', e.target.value);
+        setToggle(!toggle);
+    }
+
+    useEffect(() => {
+        document.querySelector('html').setAttribute('data-theme', themeMode);
+
+    }, [themeMode])
+
     return (
-        <>
-            <h1>Theme Toggle</h1>
-            <h1>PRAISE JESUS</h1>
-        </>
+        <div className="flex flex-col justify-center align-baseline">
+            <label className="swap swap-rote">
+                <input className="toggle"
+                    onChange={toggleThemeHandler}
+                    value={toggle === false ? 'luxury' : 'bumblebee'}
+                    type="checkbox"
+                    checked={toggle}
+                />
+            </label>
+        </div>
     );
 };
 
